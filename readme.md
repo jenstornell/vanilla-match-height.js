24673a5218168d460d6a64672c3440f763ca25b1
Jens Törnell <jens.tornell@gmail.com>
2019-11-13 11:45:46 +0100

    Bug fixes

---
+++ readme.md
@@ -0,0 +1,54 @@
+# Vanilla match height
+
+## Features
+
+- Inspired by [MatchHeight](https://github.com/liabru/jquery-match-height)
+- Vanilla javascript
+- Dependency free
+- Will recalculate on browser resize
+- Row aware
+- Accounts for `margins`, `paddings`, `borders` and `box-sizing`
+
+## Setup
+
+Put the script tags just before `</body>`.
+
+```html
+<script src="vanilla-match-height.js"></script>
+<script>
+  let equalHeight = new VanillaMatchHeight();
+  equalHeight.apply();
+</script>
+```
+
+## Usage
+
+Set `data-match-item` on elements that should match. Make sure to add a group name to them.
+
+**It make look like this:**
+
+```text
+data-match-item="hello"
+```
+
+The matching elements by group will have the same height. The height is set by the tallest element.
+
+### Example
+
+The elements containing `data-match-item="h"` will have the same height, in case they are on the same row. The same thing for `data-match-item="p"`.
+
+```html
+<div class="box">
+  <h1 data-match-item="h">A really very long heading that will break the line</h1>
+  <p data-match-item="p">A really short p tag</p>
+  <div class="button">Button</div>
+</div>
+<div class="box">
+  <h1 data-match-item="h">Short h1 tag</h1>
+  <p data-match-item="p">Macaroon fruitcake icing lollipop cake wafer. Jelly beans muffin liquorice lollipop toffee bonbon cupcake gummies. Carrot cake danish cotton candy. Danish gummies croissant halvah. Ice cream biscuit sweet roll. Gingerbread tootsie roll chupa chups tart halvah jelly-o powder powder. Pudding biscuit candy canes croissant ice cream pie. Ice cream croissant cotton candy chupa chups candy. Candy canes lollipop cheesecake oat cake powder topping lemon drops. Fruitcake wafer donut chupa chups. Lemon drops jelly beans dragée liquorice danish wafer sweet pie caramels. Lollipop halvah candy jelly chocolate.</p>
+</div>
+```
+
+## License
+
+MIT
--- vanilla-match-height.js
+++ vanilla-match-height.js
@@ -1,124 +1,100 @@
 class VanillaMatchHeight {
-	// Constructor
-	constructor() {
-		this.collection = [];
-	}
-
 	// Apply init
 	apply() {
 		window.addEventListener('DOMContentLoaded', (event) => {
-			//this.walkSections();
-			//this.walkItems();
-			let rows = this.walkRows();
-			console.log(rows);
+			let rows = this.walkPage();
+
+			if (!rows) return;
+
+			this.walkRows(rows);
 		});
 	}
 
-	walkRows() {
+	// Walk page
+	walkPage() {
 		let items = document.querySelectorAll('[data-match-item]');
 		let rows = {};
-		let i = 0;
 		let buffer = null;
 
-		items.forEach((item) => {
-			if (item.parentNode !== buffer) {
-				let top = this.getTop(item.parentElement);
-				rows[top] = rows[top] !== undefined ? rows[top] : [];
-				rows[top][i] = item.parentNode;
-				buffer = item.parentNode;
-				i++;
-			}
-		});
+		if (!items.length) return;
+
+		for (let i = 0; i < items.length; i++) {
+			let item = items[i];
+
+			if (item.parentNode === buffer) continue;
+
+			let top = this.getTop(item.parentElement);
+
+			rows[top] = rows[top] !== undefined ? rows[top] : [];
+			rows[top][i] = item.parentNode;
+
+			buffer = item.parentNode;
+		}
 
 		return rows;
 	}
 
-	getTop(element) {
-		return element.getBoundingClientRect().top;
+	// Walk rows
+	walkRows(rows) {
+		for (let top in rows) {
+			let row = rows[top];
+			let heights = this.walkGroups(row);
+			this.setHeights(row, heights);
+		}
+	}
+
+	// Walk groups
+	walkGroups(row) {
+		let heights = {};
+
+		row.forEach((group) => {
+			heights = this.walkItems(group, heights);
+		});
+
+		return heights;
 	}
 
 	// Walk items
-	walkItems() {
-		let items = document.querySelectorAll('[data-match-item]');
-		let buffer = {};
+	walkItems(group, heights) {
+		let items = group.querySelectorAll('[data-match-item]');
 
 		items.forEach((item) => {
 			let attribute = item.getAttribute('data-match-item');
 
 			if (attribute) {
-				let start = item.parentNode.getBoundingClientRect().top;
-				let inner = item.clientHeight;
-				let outer = {};
-				outer[start] = item.offsetHeight;
+				let height = this.getHeight(item);
 
-				console.log(start);
-
-				buffer[start] = buffer[start] !== undefined ? buffer[start] : 0;
-
-				if (outer[start] > buffer[start]) {
-					this.collection[attribute] = {};
-					this.collection[attribute][start] = {
-						outer: outer[start],
-						inner: inner
-					};
-					//buffer = this.collection[attribute][start].outer;
-					buffer[start] = outer[start];
+				if (heights[attribute] < height || heights[attribute] === undefined) {
+					heights[attribute] = height;
 				}
 			}
 		});
 
-		console.log(buffer);
-
-		console.log(this.collection);
+		return heights;
 	}
 
-	walkCollectionSections() {
-		//console.log(this.collection);
+	// Set heights
+	setHeights(row, heights) {
+		row.forEach((group) => {
+			let items = group.querySelectorAll('[data-match-item]');
 
-		let sections = document.querySelectorAll('[data-match-group]');
-		let section_id = 0;
+			items.forEach((item) => {
+				let attribute = item.getAttribute('data-match-item');
+				let height = heights[attribute];
 
-		sections.forEach((section) => {
-			this.walkCollectionGroups(section, section_id);
-			section_id++;
+				if (attribute) {
+					item.style.height = height + 'px';
+				}
+			});
 		});
 	}
 
-	walkCollectionGroups(section, section_id) {
-		let groups = Array.from(section.children);
-
-		groups.forEach((group) => {
-			this.walkCollectionItems(group, section_id);
-		});
+	getHeight(element) {
+		return parseInt(getComputedStyle(element).getPropertyValue('height'));
 	}
 
-	walkCollectionItems(group, section_id) {
-		let items = Array.from(group.children);
-
-		console.log(this.collection);
-
-		items.forEach((item) => {
-			let attribute = item.getAttribute('data-match-item');
-
-			if (attribute) {
-				let inner = this.collection[section_id][attribute].inner;
-				console.log(inner);
-				item = item.style.height = inner + 'px';
-			}
-
-			//console.log(section_id);
-			//console.log(item);
-		});
-	}
-
-	isEmpty(obj) {
-		return Object.keys(obj).length === 0;
+	// Get top
+	getTop(element) {
+		return element.getBoundingClientRect().top;
 	}
 }
-
-// Resize event
-// Row aware
-// Kör igen efter ajax
-// Callbacks before / after
-// By row option
-// match-group behövs ej?
